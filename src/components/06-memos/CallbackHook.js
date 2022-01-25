5b6d1a77593b31afb7c6de568218382ab56aeb12
andres <andres-jose1998@hotmail.com>
2022-01-25 14:48:20 -0500

    commit

---
+++ src/components/06-memos/CallbackHook.js
@@ -0,0 +1,35 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import '../02-useEffect/effects.css';
+import { ShowIncrement } from './ShowIncrement';
+
+export const CallbackHook = () => {
+ 
+    const [counter,setCounter] = useState(20);
+
+  //  const increment = () =>{
+   //     setCounter(counter +1 );
+   // }
+
+   const increment= useCallback( (num) => {
+        setCounter(c => c + num);
+    }, [setCounter])
+
+
+    useEffect ( () =>{
+
+    },[increment])
+ 
+    return (
+  <div>
+      <h1>useCallback Hook {counter}</h1>
+      <hr/>
+
+        <ShowIncrement increment={increment}/>
+
+  </div>
+  )
+};
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MemoHook } from './components/06-memos/MemoHook';
+import { CallbackHook } from './components/06-memos/CallbackHook';
+//import { MemoHook } from './components/06-memos/MemoHook';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { MultipleCustonHooks } from './components/03-examples/MultipleCustonHooks';
 //import { FocusScreen } from './components/04-useRef/FocusScreen';
@@ -16,7 +17,7 @@ import { MemoHook } from './components/06-memos/MemoHook';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MemoHook />
+    <CallbackHook />
   </React.StrictMode>,
   document.getElementById('root')
 );
